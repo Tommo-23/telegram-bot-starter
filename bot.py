c42e17d5d80f9e711c5fda2f557895a0b127d072
Toommo23 <142088873+Tommo-23@users.noreply.github.com>
2024-08-24 01:40:57 +0200

    kuggkk

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 
 import os
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
 
 TOKEN = os.getenv("TOKEN")
 
