53b473e309b0aaaa07e8114fc3fd8dd7d7341390
Toommo23 <142088873+Tommo-23@users.noreply.github.com>
2024-08-24 01:57:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 
 TOKEN = os.getenv("TOKEN")
 
-CHAT_DESTINAZIONE = "@TuttoModding"  
+CHAT_DESTINAZIONE = "@TuttoModding"  # Canale di destinazione
 
 CANALI_MONITORATI = {
     '@garnet_updates': 'rn13pro5g / PocoX6',
@@ -17,21 +17,20 @@ async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text('my job is to forward the new roms of your device.')
 
 async def inoltra_messaggio(update: Update, context: CallbackContext) -> None:
-    if update.message and update.message.chat:
+    if update.message:
         canale = update.message.chat.username
         if canale in CANALI_MONITORATI:
-            nome_canale = CANALI_MONITORATI[canale]
-            testo_inoltrato = f"Nuova ROM per {nome_canale}\n\n{update.message.text}"
-            await context.bot.send_message(chat_id=CHAT_DESTINAZIONE, text=testo_inoltrato)
+            await update.message.forward(chat_id=CHAT_DESTINAZIONE)
+        else:
+            print(f"Messaggio ricevuto da un canale non monitorato: {canale}")
     else:
-        # Messaggio di debug per aggiornamenti non validi
-        print("L'aggiornamento ricevuto non contiene un messaggio valido.")
+        print("L'aggiornamento ricevuto non contiene un messaggio valido. Tipo di aggiornamento:", update)
 
 def main() -> None:
     application = Application.builder().token(TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & filters.ChatType.CHANNEL, inoltra_messaggio))
+    application.add_handler(MessageHandler(filters.ChatType.CHANNEL, inoltra_messaggio))
 
     application.run_polling()
 
